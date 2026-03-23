56c5e713da72b87603fc4c68b0fd18429649311c
remarxxx7 <remarxxx7@gmail.com>
2026-03-23 13:31:21 +0300

    relocate files training

--- training_02/labirint.py
+++ training_02/labirint.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
@@ -11,27 +11,25 @@ search_input = driver.find_element(By.CSS_SELECTOR, "#search-field")
 search_input.send_keys("Python", Keys.RETURN)
 sleep(5)
 
-# или
-# search_buttom = driver.find_element(By.CSS_SELECTOR, ".b-header-b-search-e-srch-icon.b-header-e-sprite-background")
-# search_buttom.click()
+#или
+#search_buttom = driver.find_element(By.CSS_SELECTOR, ".b-header-b-search-e-srch-icon.b-header-e-sprite-background")
+#search_buttom.click()
 
-# Собрать все карточки товаров
+#Собрать все карточки товаров
 books = driver.find_elements(By.CSS_SELECTOR, "div.product-card")
 
 print(len(books))
 
 for book in books:
     title = book.find_element(By.CSS_SELECTOR, ".product-card__name").text
-    price = book.find_element(
-        By.CSS_SELECTOR, ".product-card__price-current").text
+    price = book.find_element(By.CSS_SELECTOR, ".product-card__price-current").text
     author = ""
 
     try:
-        author = book.find_element(
-            By.CSS_SELECTOR, "div.product-card__author").text
+        author = book.find_element(By.CSS_SELECTOR, "div.product-card__author").text
     except:
         author = "Автор не указан"
 
     print(author + "\t" + title + "\t" + price)
-
+    
 sleep(5)
